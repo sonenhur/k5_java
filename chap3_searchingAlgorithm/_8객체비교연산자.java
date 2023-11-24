778021c8781ac44e9a6dde8dbfb6e03a9680ee55
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-11-24 20:27:21 +0900

    Update _8객체비교연산자.java

--- chap3_searchingAlgorithm/_8객체비교연산자.java
+++ chap3_searchingAlgorithm/_8객체비교연산자.java
@@ -22,13 +22,13 @@ class PhyscData4 {
 
 class HeightOrderComparator implements Comparator<PhyscData4> {
 	public int compare(PhyscData4 p1, PhyscData4 p2) {
-		return p1.height - p2.height;
+		return p1.height - p2.height; // 키로 비교
 	}
 }
 
 public class _8객체비교연산자 {
 
-	static final Comparator<PhyscData4> HEIGHT_ORDER = new HeightOrderComparator();
+	static final Comparator<PhyscData4> HEIGHT_ORDER = new HeightOrderComparator(); // 키 순서로 정렬하는 Comparator
 
 	public static void main(String[] args) {
 		PhyscData4[] data = {
@@ -38,15 +38,18 @@ public class _8객체비교연산자 {
 				new PhyscData4("김홍길동", 172, 0.3),
 				new PhyscData4("길동", 182, 0.6),
 				new PhyscData4("길동", 167, 0.2),
-				new PhyscData4("길동", 167, 0.5), };
-		showData(data);
-		Arrays.sort(data, HEIGHT_ORDER);
-		showData(data);
+				new PhyscData4("길동", 167, 0.5),
+				};
+		
+		showData(data); // 데이터 출력
+		Arrays.sort(data, HEIGHT_ORDER); // 키 순서로 정렬
+		showData(data); // 정렬된 데이터 출력
+
 		PhyscData4 key = new PhyscData4("길동", 167, 0.2);
-		int idx = Arrays.binarySearch(data, key, HEIGHT_ORDER);
+		int idx = Arrays.binarySearch(data, key, HEIGHT_ORDER); // 키 순서로 이진 탐색
 
 		System.out.println("\nArrays.binarySearch(): result = " + idx);
-		System.out.println("원하는 값의 위치는 " + idx +"(+1)");
+		System.out.println("원하는 값의 위치는 " + idx + "(+1)");
 	}
 
 	static void showData(PhyscData4[] arr) {
