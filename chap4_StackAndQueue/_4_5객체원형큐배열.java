6a05f2021900da3a9882dd758e776770ee6b9967
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-11-30 23:18:36 +0900

    Update _4_5객체원형큐배열.java

--- chap4_StackAndQueue/_4_5객체원형큐배열.java
+++ chap4_StackAndQueue/_4_5객체원형큐배열.java
@@ -7,154 +7,182 @@ package chap4_StackAndQueue;
 import java.util.Random;
 import java.util.Scanner;
 
-class Point4 {
-	private int ix;
-	private int iy;
+//2차원 점을 나타내는 클래스
+class Point4 { 
+    private int ix;
+    private int iy;
 
-	public Point4(int x, int y) {
-		ix = x;
-		iy = y;
-	}
+    public Point4(int x, int y) {
+        ix = x;
+        iy = y;
+    }
 
-	@Override
-	public String toString() {
-		return "<" + ix + ", " + iy + ">";
-	}
+    @Override
+    public String toString() {
+        return "<" + ix + ", " + iy + ">";
+    }
 
-	public int getX() {
-		return ix;
-	}
+    public int getX() {
+        return ix;
+    }
 
-	public int getY() {
-		return iy;
-	}
+    public int getY() {
+        return iy;
+    }
 
-	public void setX(int x) {
-		ix = x;
-	}
+    public void setX(int x) {
+        ix = x;
+    }
 
-	public void setY(int y) {
-		iy = y;
-	}
+    public void setY(int y) {
+        iy = y;
+    }
 
-	@Override
-	public boolean equals(Object p) {
-		if ((this.ix == ((Point4) p).ix) && (this.iy == ((Point4) p).iy))
-			return true;
-		else
-			return false;
-	}
+    // 두 점이 같은지 비교하기 위한 equals 오버라이드
+    @Override
+    public boolean equals(Object p) {
+        if ((this.ix == ((Point4) p).ix) && (this.iy == ((Point4) p).iy))
+            return true;
+        else
+            return false;
+    }
 }
 
+// 객체를 원형 큐로 저장하는 클래스
 public class _4_5객체원형큐배열 {
 
-	static int QUEUE_SIZE = 0;
-	Point4[] que;// 배열로 객체원형 큐 구현
-	int front, rear;
-	int num;
-	static boolean isEmpty;
+    static int QUEUE_SIZE = 0;
+    Point4[] que;
+    int front, rear; // 큐의 시작과 끝을 가리키는 포인터
+    int num; // 큐 안의 요소 수를 추적하는 변수
+    static boolean isEmpty;
 
-	// --- 실행시 예외: 큐가 비어있음 ---//
-	public class EmptyQueueException extends RuntimeException {
-		public EmptyQueueException() {
-		}
-	}
+    public class EmptyQueueException extends RuntimeException {
+        public EmptyQueueException() {
+        }
+    }
 
-	// --- 실행시 예외: 큐가 가득 찼음 ---//
-	public class OverflowQueueException extends RuntimeException {
-		public OverflowQueueException() {
-		}
-	}
+    public class OverflowQueueException extends RuntimeException {
+        public OverflowQueueException() {
+        }
+    }
 
-	public _4_5객체원형큐배열(int count) {
+    public _4_5객체원형큐배열(int count) {
+        QUEUE_SIZE = count;
+        que = new Point4[QUEUE_SIZE];
+        front = rear = num = 0;
+    }
 
-	}
+    // Point4를 원형 큐에 넣는 메서드 (Enqueue)
+    void push(Point4 item) {
+        if (num >= QUEUE_SIZE)
+            throw new OverflowQueueException(); // 큐가 가득 찼을 때 예외 발생
+        num++;
+        que[rear++] = item;
+        if (rear == QUEUE_SIZE)
+            rear = 0; // 끝에 도달하면 처음으로 돌아감
+    }
 
-	void push(Point4 it) {
+    // 원형 큐에서 Point4를 빼는 메서드 (Dequeue)
+    Point4 pop() {
+        if (num <= 0)
+            throw new EmptyQueueException(); // 큐가 비어있을 때 예외 발생
+        num--;
+        Point4 item = que[front++];
+        if (front == QUEUE_SIZE)
+            front = 0; // 끝에 도달하면 처음으로 돌아감
+        return item;
+    }
 
-	}
+    // 원형 큐를 비우는 메서드
+    void clear() {
+        for (int i = 0; i < que.length; i++) {
+            que[i] = null;
+            front = rear = 0;
+        }
+        isEmpty = true;
+        System.out.println("원형 큐가 비었습니다.");
+    }
 
-	Point4 pop() {
+    // 큐의 크기를 반환하는 메서드
+    public int getCapacity() {
+        return QUEUE_SIZE;
+    }
 
-	}
+    // 큐에 들어있는 요소 수를 반환하는 메서드
+    public int size() {
+        return num;
+    }
 
-	void clear() {
-		for (int i = 0; i < que.length; i++) {
-			que[i] = null;
-			front = rear = 0;
-		}
-		isEmpty = true;
-		System.out.println("원형 큐가 비었습니다.");
-	}
+    // 큐의 요소를 출력하는 메서드
+    void dump() {
+        if (num <= 0)
+            System.out.println("큐가 비어 있습니다.");
+        else
+            for (int i = 0; i < num; i++)
+                System.out.print(que[(i + front) % QUEUE_SIZE] + " ");
+        System.out.println();
+    }
 
-	// --- 큐의 크기를 반환 ---//
-	public int getCapacity() {
-		return QUEUE_SIZE;
-	}
+    // 큐의 맨 앞에 있는 요소를 확인하는 메서드
+    Point4 peek() {
+        if (num <= 0)
+            throw new EmptyQueueException(); // 큐가 비어있을 때 예외 발생
+        return que[front];
+    }
 
-	// --- 큐에 쌓여 있는 데이터 개수를 반환 ---//
-	public int size() {
-		return num;
-	}
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        _4_5객체원형큐배열 oq = new _4_5객체원형큐배열(4); // 최대 크기가 4인 큐 생성
+        Random random = new Random();
+        int rndx, rndy;
+        Point4 p;
+        while (true) {
+            System.out.println(" ");
+            System.out.printf("현재 데이터 개수: %d / %d\n", oq.size(), oq.getCapacity());
+            System.out.print("(1)인큐　(2)디큐　(3)피크　(4)덤프　(0)종료: ");
+            int menu = stdIn.nextInt();
+            switch (menu) {
+                case 1: // 인큐
+                    rndx = random.nextInt(20);
+                    rndy = random.nextInt(20);
+                    System.out.print("입력데이터: (" + rndx + ", " + rndy + ")");
+                    p = new Point4(rndx, rndy);
+                    try {
+                        oq.push(p);
+                    } catch (_4_5객체원형큐배열.OverflowQueueException e) {
+                        System.out.println("큐가 가득차있습니다.");
+                    }
+                    break;
 
-	public void dump() {
+                case 2: // 디큐
+                    try {
+                        p = oq.pop();
+                        System.out.println("디큐한 데이터는 " + p + "입니다.");
+                    } catch (_4_5객체원형큐배열.EmptyQueueException e) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
 
-	}
+                case 3: // 피크
+                    try {
+                        p = oq.peek();
+                        System.out.println("피크한 데이터는 " + p + "입니다.");
+                    } catch (_4_5객체원형큐배열.EmptyQueueException e) {
+                        System.out.println("큐가 비어 있습니다.");
+                    }
+                    break;
 
-	public Point4 peek() throws EmptyQueueException {
+                case 4: // 덤프
+                    oq.dump();
+                    break;
 
-	}
-
-	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
-		_4_5객체원형큐배열 oq = new _4_5객체원형큐배열(4); // 최대 64개를 인큐할 수 있는 큐
-		Random random = new Random();
-		int rndx = 0, rndy = 0;
-		Point4 p = null;
-		while (true) {
-			System.out.println(" "); // 메뉴 구분을 위한 빈 행 추가
-			System.out.printf("현재 데이터 개수: %d / %d\n", oq.size(), oq.getCapacity());
-			System.out.print("(1)인큐　(2)디큐　(3)피크　(4)덤프　(0)종료: ");
-			int menu = stdIn.nextInt();
-			switch (menu) {
-				case 1: // 인큐
-
-					rndx = random.nextInt(20);
-
-					rndy = random.nextInt(20);
-					System.out.print("입력데이터: (" + rndx + ", " + rndy + ")");
-					p = new Point4(rndx, rndy);
-					try {
-						oq.push(p);
-					} catch (objectQueue2.OverflowQueueException e) {
-						System.out.println("stack이 가득찼있습니다.");
-					}
-					break;
-
-				case 2: // 디큐
-					try {
-						p = oq.pop();
-						System.out.println("디큐한 데이터는 " + p + "입니다.");
-					} catch (objectQueue2.EmptyQueueException e) {
-						System.out.println("큐가 비어 있습니다.");
-					}
-					break;
-
-				case 3: // 피크
-					try {
-						p = oq.peek();
-						System.out.println("피크한 데이터는 " + p + "입니다.");
-					} catch (objectQueue2.EmptyQueueException e) {
-						System.out.println("큐가 비어 있습니다.");
-					}
-					break;
-
-				case 4: // 덤프
-					oq.dump();
-					break;
-				default:
-					break;
-			}
-		}
-	}
+                case 0: // 종료
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
