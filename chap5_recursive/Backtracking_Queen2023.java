0f805db65d85698272db9d07659fe7d4bb94d0eb
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-12-07 22:46:02 +0900

    답이 하나만 나온 케이스

--- chap5_recursive/Backtracking_Queen2023.java
+++ chap5_recursive/Backtracking_Queen2023.java
@@ -1,36 +1,36 @@
 package chap5_recursive;
+
 //https://www.geeksforgeeks.org/n-queen-problem-backtracking-3/?ref=lbp
-
 //모든 해가 나오는 버젼 만들기
-
+// 필요한 클래스 가져오기
 class Stack3 {
-    private final Point[] array;
-    private final int maxSize;
-    private int top;
+    private final Point[] array; // 포인트를 저장하는 배열
+    private final int maxSize; // 스택의 최대 크기
+    private int top; // 최상위 요소의 인덱스
 
+    // 스택 생성자: 주어진 크기로 스택을 초기화함
     public Stack3(int size) {
-        // 스택 생성자: 크기를 받아 스택을 초기화함
         maxSize = size;
         array = new Point[maxSize];
-        top = -1; // -1로 초기화하여 빈 스택을 나타냄
+        top = -1; // 빈 스택을 나타내기 위해 -1로 초기화
     }
 
+    // 스택에 원소 추가
     public void push(Point p) {
-        // 스택에 원소 추가
-        array[++top] = p;
+        array[++top] = p; // top을 증가시키고 해당 위치에 포인트를 추가
     }
 
+    // 스택에서 원소 제거하고 반환
     public Point pop() {
-        // 스택에서 원소 제거하고 반환
-        return array[top--];
+        return array[top--]; // 최상위 원소를 반환하고 top을 감소시킴
     }
 
+    // 스택이 비어있는지 여부 반환
     public boolean isEmpty() {
-        // 스택이 비어있는지 여부 반환
         return (top == -1);
     }
 
-    // 스택의 top에 있는 값 반환
+    // 최상위 원소의 인덱스 반환
     public int getTop() {
         return top;
     }
@@ -41,55 +41,52 @@ class Stack3 {
     }
 }
 
+// x 및 y 좌표를 가지는 포인트를 나타내는 클래스
 class Point {
     int x, y;
 
+    // 좌표를 초기화하는 생성자
     Point(int x, int y) {
-        // 좌표를 나타내는 Point 클래스 생성자
         this.x = x;
         this.y = y;
     }
 }
 
+// 백트래킹을 사용하여 8-퀸 문제를 해결하는 메인 클래스
 public class Backtracking_Queen2023 {
-
+    // 8-퀸 문제를 해결하는 메서드
     public static void solveQueen(int[][] d) {
-        int count = 0; // 퀸 배치 갯수
-        int ix = 0, iy = 0; // 현재 행 ix, 열 iy
-        Stack3 st = new Stack3(100); // 100개를 저장할 수 있는 스택 생성
-        Point p = new Point(ix, iy); // 현 위치를 객체로 만들고
-        d[ix][iy] = 1; // 현 위치에 퀸을 넣었다는 표시를 하고
+        int count = 0; // 배치된 퀸 수
+        int ix = 0, iy = 0; // 현재 행 (ix) 및 열 (iy)
+        Stack3 st = new Stack3(100); // 포인트를 저장하는 스택 생성 (최대 크기: 100)
+        Point p = new Point(ix, iy); // 현재 위치에 대한 포인트 객체 생성
+        d[ix][iy] = 1; // 현재 위치에 퀸을 표시
         count++;
-        st.push(p); // 스택에 현 위치 객체를 push
+        st.push(p); // 현재 위치를 스택에 푸시
         ix++;
-
-        // Use a flag to indicate whether a solution is found
-        boolean solutionFound = false;
 
         while (true) {
             if (st.isEmpty()) {
-                // 스택이 비어있으면 모든 가능성을 탐색한 것이므로 종료
+                // 스택이 비어있으면 모든 가능성을 탐색했으므로 종료
                 break;
             }
-
             if (ix == 8) {
                 // 8개의 퀸을 성공적으로 배치한 경우
-                solutionFound = true;
                 showQueens(d);
                 Point prevPoint = st.pop();
                 ix--; // 이전 행으로 이동
-                count--; // 퀸 갯수 감소
-                d[prevPoint.x][prevPoint.y] = 0; // 퀸을 제거
+                count--; // 퀸 수 감소
+                d[prevPoint.x][prevPoint.y] = 0; // 퀸 제거
                 iy = prevPoint.y + 1; // 이전 행의 다음 열로 이동
             } else if ((iy = nextMove(d, ix, iy)) == -1) {
-                // 현재 행에서 더 이상 유효한 이동이 없을 경우
+                // 현재 행에서 더 이상 유효한 이동이 없는 경우
                 Point prevPoint = st.pop();
                 ix--; // 이전 행으로 이동
-                count--; // 퀸 갯수 감소
-                d[prevPoint.x][prevPoint.y] = 0; // 퀸을 제거
+                count--; // 퀸 수 감소
+                d[prevPoint.x][prevPoint.y] = 0; // 퀸 제거
                 iy = prevPoint.y + 1; // 이전 행의 다음 열로 이동
             } else {
-                // 유효한 이동이 있으면 퀸을 배치
+                // 유효한 이동이 있는 경우 퀸 배치
                 p = new Point(ix, iy);
                 d[ix][iy] = 1;
                 count++;
@@ -98,29 +95,25 @@ public class Backtracking_Queen2023 {
                 iy = 0; // 다음 행의 첫 열로 이동
             }
         }
-
-        if (!solutionFound) {
-            System.out.println("No solution found.");
-        }
     }
 
+    // 스택의 내용을 출력하는 메서드
     public static void dumpStack(Stack3 stack) {
-        // 스택 내용을 출력하는 메서드
         for (int i = 0; i <= stack.getTop(); i++) {
             System.out.println("[" + stack.getArray()[i].x + ", " + stack.getArray()[i].y + "]");
         }
         System.out.println();
     }
 
+    // 주어진 행에 퀸을 배치할 수 있는지 확인하는 메서드
     public static boolean checkRow(int[][] d, int crow) {
-        // 배열 d에서 행 crow에 퀸을 배치할 수 있는지 확인
         for (int i = 0; i < d.length; i++) {
             if (crow < 0 || crow >= d.length) {
-                // 행 인덱스가 배열 범위를 벗어나면 배치할 수 없음
+                // 행 인덱스가 배열 범위를 벗어나면 퀸을 배치할 수 없음
                 return false;
             }
             if (d[crow][i] == 1) {
-                // 해당 행에 이미 퀸이 배치되어 있음
+                // 해당 행에 이미 퀸이 배치되어 있으면 퀸을 배치할 수 없음
                 return false;
             }
         }
@@ -128,11 +121,11 @@ public class Backtracking_Queen2023 {
         return true;
     }
 
+    // 주어진 열에 퀸을 배치할 수 있는지 확인하는 메서드
     public static boolean checkCol(int[][] d, int ccol) {
-        // 배열 d에서 열 ccol에 퀸을 배치할 수 있는지 확인
         for (int i = 0; i < d.length; i++) {
             if (d[i][ccol] == 1) {
-                // 해당 열에 이미 퀸이 배치되어 있음
+                // 해당 열에 이미 퀸이 배치되어 있으면 퀸을 배치할 수 없음
                 return false;
             }
         }
@@ -140,12 +133,12 @@ public class Backtracking_Queen2023 {
         return true;
     }
 
+    // 주어진 위치에 남서 방향 대각선에 퀸을 배치할 수 있는지 확인하는 메서드
     public static boolean checkDiagSW(int[][] d, int cx, int cy) {
-        // 배열 d에서 행 cx, 열 cy에 퀸을 남서, 북동 대각선으로 배치할 수 있는지 확인
         int x = cx;
         int y = cy;
 
-        // 북동
+        // 남동 방향 대각선 확인
         while (x >= 0 && x < d.length && y >= 0 && y < d.length) {
             if (d[x][y] == 1) return false;
             x--;
@@ -155,7 +148,7 @@ public class Backtracking_Queen2023 {
         x = cx;
         y = cy;
 
-        // 남서
+        // 북서 방향 대각선 확인
         while (x >= 0 && x < d.length && y >= 0 && y < d.length) {
             if (d[x][y] == 1) return false;
             x++;
@@ -164,12 +157,12 @@ public class Backtracking_Queen2023 {
         return true;
     }
 
+    // 주어진 위치에 남동 방향 대각선에 퀸을 배치할 수 있는지 확인하는 메서드
     public static boolean checkDiagSE(int[][] d, int cx, int cy) {
-        // 배열 d에서 행 cx, 열 cy에 퀸을 남동, 북서 대각선으로 배치할 수 있는지 확인
         int x = cx;
         int y = cy;
 
-        // 북서
+        // 북서 방향 대각선 확인
         while (x >= 0 && x < d.length && y >= 0 && y < d.length) {
             if (d[x][y] == 1) return false;
             x--;
@@ -179,7 +172,7 @@ public class Backtracking_Queen2023 {
         x = cx;
         y = cy;
 
-        // 남동
+        // 남동 방향 대각선 확인
         while (x >= 0 && y >= 0 && x < d.length && y < d.length) {
             if (d[x][y] == 1) return false;
             x++;
@@ -188,23 +181,23 @@ public class Backtracking_Queen2023 {
         return true;
     }
 
+    // 주어진 위치에 퀸을 배치할 수 있는지 여부 확인
     public static boolean checkMove(int[][] d, int x, int y) {
-        // 특정 위치에 퀸을 배치할 수 있는지 여부 확인
         return checkRow(d, x) && checkCol(d, y) && checkDiagSW(d, x, y) && checkDiagSE(d, x, y);
     }
 
+    // 다음 이동 가능한 열을 반환, 이동할 열이 없으면 -1 반환
     public static int nextMove(int[][] d, int row, int col) {
-        // 배열 d에서 현재 위치(row, col)에 대하여 다음에 이동할 위치 nextCol을 반환, 이동이 불가능하면 -1을 반환
         for (int nextCol = col + 1; nextCol < d.length; nextCol++) {
             if (checkMove(d, row, nextCol)) {
-                return nextCol; // 다음 열로 이동 가능한 경우 해당 열을 반환
+                return nextCol; // 다음 열로 이동 가능한 경우 해당 열 반환
             }
         }
-        return -1; // 더 이상 이동할 열이 없는 경우
+        return -1; // 이동할 열이 더 이상 없는 경우
     }
 
+    // 현재 상태에서의 퀸 배치를 출력하는 메서드
     static void showQueens(int[][] data) {
-        // 현재 상태에서의 퀸 배치를 출력하는 메서드
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
                 if (data[i][j] == 1) {
@@ -218,13 +211,17 @@ public class Backtracking_Queen2023 {
         System.out.println();
     }
 
+    // 메인 메서드: 프로그램 실행
     public static void main(String[] args) {
-        int row = 8, col = 8;
-        int[][] data = new int[row][col];
+        int row = 8, col = 8; // 체스보드의 크기
+        int[][] data = new int[row][col]; // 체스보드를 나타내는 2D 배열
+
+        // 모든 원소를 0으로 초기화
         for (int i = 0; i < data.length; i++)
             for (int j = 0; j < data[0].length; j++)
                 data[i][j] = 0;
 
+        // 8-퀸 문제를 해결하고 해결책을 출력
         solveQueen(data);
     }
 }
