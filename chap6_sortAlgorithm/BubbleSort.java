bf61ffe19a01a348425eb6357113409a12dc42a7
sonenhur <sonen1125@gmail.com>
2023-12-12 17:57:12 +0900

    20231212

--- chap6_sortAlgorithm/BubbleSort.java
+++ chap6_sortAlgorithm/BubbleSort.java
@@ -1,24 +1,25 @@
-public static void main(String[] args) {
+package chap6_sortAlgorithm;
 
-    class BubbleSort {
-        static void swap(int[] a, int idx1, int idx2) {
-            int t = a[idx1];
-            a[idx1] = a[idx2];
-            a[idx2] = t;
-        }
+import java.util.Scanner;
 
-        static void bubbleSort(int[], int n) {
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = n - 1; j > i; j--) {
-                    if (a[j - 1] > a[j])
-                        swap(a, j - 1, j);
-                }
+class BubbleSort {
+    static void swap(int[] a, int idx1, int idx2) {
+        int t = a[idx1];
+        a[idx1] = a[idx2];
+        a[idx2] = t;
+    }
+
+    static void bubbleSort(int[] a, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = n - 1; j > i; j--) {
+                if (a[j - 1] > a[j])
+                    swap(a, j - 1, j);
             }
         }
+    }
 
-        public static void main(String[] args) {
-            Scanner stdIn = new Scanner(System.in);
-
+    public static void main(String[] args) {
+        try (Scanner stdIn = new Scanner(System.in)) {
             System.out.println("버블 정렬(버전1)");
             System.out.println("요솟수: ");
             int nx = stdIn.nextInt();
--- chap6_sortAlgorithm/Chap6_Test_MergeSort.java
+++ chap6_sortAlgorithm/Chap6_Test_MergeSort.java
@@ -1,43 +1,86 @@
+package chap6_sortAlgorithm;
 
-package chap6_sortAlgorithm; // PhyscData 클래스를 Comparable로 수정해야 한다. // stack을
- // 이용한 객체들의 non-recusrive MergeSort 구현
- 
- public class Chap6_Test_MergeSort { package Chap6_Sorting; //MergeSort() 구현
- // 소스 코드를 사용하여 객체들의 정렬 소스코드로 수정한다. import java.util.Arrays;
- 
- public class MergeSortObjects {
- 
- // --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---// static void merge(PhyscData[] a,
- int lefta, int righta, int leftb, int rightb ) {
- 
- }
- 
- // --- 퀵 정렬(비재귀 버전)---//
- static void MergeSort(PhyscData[] a, int left, int right) {
- int mid = (left+right)/2; 
- if (left == right) 
-	 return;
- MergeSort(a, left, mid);
- MergeSort(a, mid+1, right);
- merge(a, left, mid, mid+1, right);
- return;
- }
- 
- public static void main(String[] args) {
-	 PhyscData[] x = {
-			 new PhyscData("강민하", 162, 0.3),
-			 new PhyscData("김찬우", 173, 0.7), 
-			 new PhyscData("박준서", 171, 2.0), 
-			 new PhyscData("유서범", 171, 1.5),
-			 new PhyscData("이수연", 168, 0.4), 
-			 new PhyscData("장경오", 171, 1.2),
-			 new PhyscData("황지안", 169, 0.8), };
-	 int nx = x.length;
- 
- MergeSort(x, 0, nx - 1); // 배열 x를 퀵정렬 System.out.println("오름차순으로 정렬했습니다.");
- 
- System.out.println("■ 신체검사 리스트 ■"); System.out.println(" 이름     키  시력");
- System.out.println("------------------"); for (int i = 0; i < x.length; i++)
- System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision); } }
- 
- }
+// PhyscData 클래스를 Comparable로 수정해야 한다.
+// stack을 이용한 객체들의 non-recursive MergeSort 구현
+
+public class Chap6_Test_MergeSort {
+    // 작업용 배열 buff 선언
+    private static PhyscData[] buff;
+
+    // --- 배열 요소 a[idx1]와 a[idx2]의 값을 교환 ---//
+    static void merge(PhyscData[] a, int lefta, int righta, int leftb, int rightb) {
+        int i = lefta, j = leftb, k = 0;
+
+        while (i <= righta && j <= rightb) {
+            buff[k++] = (a[i].compareTo(a[j]) <= 0) ? a[i++] : a[j++];
+        }
+        while (i <= righta) {
+            buff[k++] = a[i++];
+        }
+
+        while (j <= rightb) {
+            buff[k++] = a[j++];
+        }
+
+        for (i = lefta, k = 0; i <= rightb; i++, k++) {
+            a[i] = buff[k];
+        }
+    }
+
+    // --- MergeSort(비재귀 버전)---//
+    static void MergeSort(PhyscData[] a, int n) {
+        int size;
+        int lefta, righta, leftb, rightb;
+
+        for (size = 1; size < n; size *= 2) {
+            lefta = 0;
+            while (lefta + size < n) {
+                righta = lefta + size - 1;
+                leftb = lefta + size;
+                rightb = Math.min(leftb + size - 1, n - 1);
+                merge(a, lefta, righta, leftb, rightb);
+                lefta = rightb + 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PhyscData[] x = {
+                new PhyscData("강민하", 162, 0.3),
+                new PhyscData("김찬우", 173, 0.7),
+                new PhyscData("박준서", 171, 2.0),
+                new PhyscData("유서범", 171, 1.5),
+                new PhyscData("이수연", 168, 0.4),
+                new PhyscData("장경오", 171, 1.2),
+                new PhyscData("황지안", 169, 0.8),
+        };
+        int nx = x.length;
+        buff = new PhyscData[nx]; // 작업용 배열 buff 크기 조정
+
+        MergeSort(x, nx); // 배열 x를 MergeSort로 정렬
+
+        System.out.println("오름차순으로 정렬했습니다.");
+        System.out.println("■ 신체검사 리스트 ■");
+        System.out.println(" 이름     키  시력");
+        System.out.println("------------------");
+        for (PhyscData physcData : x)
+            System.out.printf("%-8s%3d%5.1f\n", physcData.name, physcData.height, physcData.vision);
+    }
+
+    static class PhyscData implements Comparable<PhyscData> {
+        String name;
+        int height;
+        double vision;
+
+        PhyscData(String name, int height, double vision) {
+            this.name = name;
+            this.height = height;
+            this.vision = vision;
+        }
+
+        @Override
+        public int compareTo(PhyscData o) {
+            return this.height - o.height;
+        }
+    }
+}
