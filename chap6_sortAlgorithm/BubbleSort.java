38b4003a9a45241dee95b4d5886c6dcf609e536a
sonenhur <148170713+sonenhur@users.noreply.github.com>
2023-12-27 10:56:20 +0900

    Update BubbleSort.java

--- chap6_sortAlgorithm/BubbleSort.java
+++ chap6_sortAlgorithm/BubbleSort.java
@@ -1,21 +1,7 @@
 package chap6_sortAlgorithm;
-<<<<<<< HEAD
 
 import java.util.Scanner;
 
-=======
->>>>>>> e3ddfa489db8c865c3c305805bdd3e85bac65d6e
-
-import java.util.Scanner;
-
-<<<<<<< HEAD
-        static void bubbleSort(int[] a, int n) {
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = n - 1; j > i; j--) {
-                    if (a[j - 1] > a[j])
-                        swap(a, j - 1, j);
-                }
-=======
 class BubbleSort {
     static void swap(int[] a, int idx1, int idx2) {
         int t = a[idx1];
@@ -28,38 +14,28 @@ class BubbleSort {
             for (int j = n - 1; j > i; j--) {
                 if (a[j - 1] > a[j])
                     swap(a, j - 1, j);
->>>>>>> e3ddfa489db8c865c3c305805bdd3e85bac65d6e
             }
         }
     }
 
-<<<<<<< HEAD
-        public static void main(String[] args) {
-            try (Scanner stdIn = new Scanner(System.in)) {
-				System.out.println("버블 정렬(버전1)");
-				System.out.println("요솟수: ");
-				int nx = stdIn.nextInt();
-				int[] x = new int[nx];
-
-				for (int i = 0; i < nx; i++) {
-				    System.out.println("x[" + i + "]: ");
-				    x[i] = stdIn.nextInt();
-				}
-=======
     public static void main(String[] args) {
         try (Scanner stdIn = new Scanner(System.in)) {
             System.out.println("버블 정렬(버전1)");
             System.out.println("요솟수: ");
             int nx = stdIn.nextInt();
             int[] x = new int[nx];
->>>>>>> e3ddfa489db8c865c3c305805bdd3e85bac65d6e
 
-				bubbleSort(x, nx);
+            for (int i = 0; i < nx; i++) {
+                System.out.println("x[" + i + "]: ");
+                x[i] = stdIn.nextInt();
+            }
 
-				System.out.println("오름차순 정렬");
-				for (int i = 0; i < nx; i++) {
-				    System.out.println("x[" + i + "]=" + x[i]);
-				}
-			}
+            bubbleSort(x, nx);
+
+            System.out.println("오름차순 정렬");
+            for (int i = 0; i < nx; i++) {
+                System.out.println("x[" + i + "]=" + x[i]);
+            }
         }
     }
+}
