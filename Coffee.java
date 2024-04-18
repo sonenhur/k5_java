ad12786ed279ed0bb2d5148a31412a2e2b331f34
sonenhur <sonen1125@gmail.com>
2024-04-18 10:41:02 +0900

    20240418

--- Coffee.java
+++ Coffee.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 
-public class Coffee {
-	private String name;
-	private int price;
+public class Coffee implements Comparable<Coffee> {
+	private final String name;
+	private final int price;
 
 	public Coffee(String name, int price) {
 		this.name = name;
@@ -17,15 +17,28 @@ public class Coffee {
 		return this.price;
 	}
 
+	@Override
+	public int compareTo(Coffee otherCoffee) {
+		// Compare coffees based on their prices
+		return Integer.compare(this.price, otherCoffee.getPrice());
+	}
+
+	@Override
+	public String toString() {
+		return "Coffee{" +
+				"name='" + name + '\'' +
+				", price=" + price +
+				'}';
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Coffee[] coffees = new Coffee[] { new Coffee("Americano", 3500), new Coffee("Green tea Latte", 5500),
 				new Coffee("Vanilla Latte", 4500), new Coffee("Espresso", 3000) };
 
-		Arrays.sort(coffees); // ERROR
+		Arrays.sort(coffees); // Now it should work
 		for (Coffee coffee : coffees) {
 			System.out.println(coffee.toString());
 		}
 	}
-
 }
