eecee4adf38cd98fc9e08261f8f96b6845618cd6
sonenhur <sonen1125@gmail.com>
2024-08-18 15:55:38 +0900

    review

--- part1/chap02/Test007.java
+++ part1/chap02/Test007.java
@@ -1,18 +1,14 @@
 package part1.chap02;
 
 public class Test007 {
-
-	public static void main(String[] args) {
-
-		byte a = 23;
-		int b = (int) a; // type casting(형변환)
-		// explicit 명시적 형변환 <-> implicit 묵시적 형변환(자동)
-		System.out.println(b);
-
-		int c = 23;
-		byte d = (byte) c;
-		System.out.println(d);
-		// 작성자가 강제시킴. (숫자 커지면 결과 달라지니 남용하지 말 것)
-	}
-
+    public static void main(String[] args) {
+        byte a = 23;
+        int b = (int) a; // type casting(형변환)
+        // explicit 명시적 형변환 <-> implicit 묵시적 형변환(자동)
+        System.out.println(b);
+        int c = 23;
+        byte d = (byte) c;
+        System.out.println(d);
+        // 작성자가 강제시킴. (숫자 커지면 결과 달라지니 남용하지 말 것)
+    }
 }
